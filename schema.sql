7d7061570bff1713feb5ee36b092f695645f45c0
Beatriz Rodriguez <bea.rodriguez3000@gmail.com>
2019-08-10 12:58:02 -0500

    add finished schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,18 @@
+-- Database: ETL_project
+
+-- DROP DATABASE "ETL_project";
+
+CREATE DATABASE "ETL_project"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+-- CREATE econ TABLE
+
 DROP TABLE IF EXISTS econ;
 
 CREATE TABLE econ (
@@ -19,6 +34,31 @@ CREATE TABLE econ (
 );
 
 SELECT * FROM econ;
+
+-- IMPORT econ_df.csv INTO econ 
+
+COPY econ_df FROM '/resources/ETL_Project_Group2/econ_df.csv' CSV HEADER
+
+--FILTER econ TABLE 
+
+ALTER TABLE econ
+DROP "2007", DROP "2008", DROP "2009",
+DROP "2010", DROP "2011", DROP "2012",
+DROP "2013",DROP "2014", DROP "2015", 
+DROP "2016", DROP "NUMBER"
+
+-- ALTER TABLE strings INTO floats
+
+ALTER TABLE econ ADD "2017 GDP" FLOAT;
+UPDATE econ SET "2017 GDP" = CAST("2017" AS FLOAT);
+
+-- DROP extra column
+
+ALTER TABLE econ
+DROP "2017", DROP newcol
+SELECT * FROM econ
+
+-- CREATE demo TABLE
 
 DROP TABLE IF EXISTS demo;
 
@@ -49,6 +89,18 @@ CREATE TABLE demo (
 
 SELECT * FROM demo;
 
+-- IMPORT demo_df.csv INTO econ
+
+COPY demo_df FROM '/resources/ETL_Project_Group2/demo_df.csv' CSV HEADER
+
+--FILTER demo TABLE
+
+ALTER TABLE demo 
+DROP "NUMBER", DROP "2010 Total Population",
+DROP "2018 Population Estimate";
+
+SELECT * FROM demo
+
 -- Table: public.cities
 
 -- DROP TABLE public.cities;
@@ -69,12 +121,90 @@ TABLESPACE pg_default;
 ALTER TABLE public.cities
     OWNER to postgres;
 
+-- IMPORT cities.csv into TABLE
+
 COPY cities FROM '/resources/ETL_Project_Group2/cities.csv' CSV HEADER;
 
 SELECT * FROM cities;
 
+-- FILTER cities TABLE
+
 ALTER TABLE cities
 DROP _2012metro;
+
+-- CREATE housing TABLE 
+
+DROP TABLE IF EXISTS housing;
+
+CREATE TABLE housing (
+	"Date" VARCHAR(30),
+	"Atlanta" FLOAT,
+	"Boston" FLOAT,
+	"Charlotte" FLOAT,
+	"Chicago" FLOAT,
+	"Cleveland" FLOAT,
+	"Dallas" FLOAT,
+	"Denver" FLOAT,
+	"Detroit" FLOAT,
+	"Las Vegas" FLOAT,
+	"Los Angeles" FLOAT,
+	"Miami" FLOAT,
+	"Minneapolis" FLOAT,
+	"New York City" FLOAT,
+	"Pheonix" FLOAT,
+	"Portland" FLOAT,
+	"San Diego" FLOAT,
+	"San Francisco" FLOAT,
+	"Seattle" FLOAT,
+	"Tampa Bay Area" FLOAT,
+	"Washington" FLOAT
+);
+
+-- IMPORT housedata.csv INTO TABLE housing
+
+COPY housing FROM '/resources/ETL_Project_Group2/housedata.csv' CSV HEADER;
+
+-- CREATE attendance TABLE 
+
+CREATE TABLE attendance (
+	"City" VARCHAR(50),
+	"Team" VARCHAR(50),
+	"League" VARCHAR(50),
+	"Year" INT,
+	"Home_GMS" INT,
+	"Home_AVG" TEXT,
+	"Home_PCT" TEXT,
+	"Road_GMS" INT,
+	"Road_AVG" TEXT,
+	"Road_PCT" FLOAT,
+	"Over_GMS" INT,
+	"Over_AVG" TEXT,
+	"Over_PCT" FLOAT
+);
+
+-- IMPORT attendance.csv INTO TABLE attendance 
+
+COPY attendance FROM '/resources/ETL_Project_Group2/attendance/attendance.csv' CSV HEADER;
+
+-- CREATE TABLE sport_city
+
+DROP TABLE IF EXISTS sport_city;
+
+CREATE TABLE sport_city (
+	"Metro" TEXT,
+	"City" TEXT,
+	"State" VARCHAR(5),
+	"NFL" TEXT,
+	"MLB" TEXT,
+	"NBA" TEXT,
+	"NHL" TEXT
+);
+
+-- IMPORT sport_city.csv INTO TABLE sport_city
+
+COPY sport_city FROM '/resources/ETL_Project_Group2/attendance/sport_city.csv' CSV HEADER;
+
+-- CREATE INNER JOIN FOR cities, demo, housing, attendance and econ TABLES
 
 SELECT d."City", d."State",
 		d."2017 Population Estimate",
@@ -91,13 +221,25 @@ SELECT d."City", d."State",
 		d."Two or More Races", d."Hispanic",
 		d."White Alone (Not Hispanic or Latino)", 
 		d."Veterans",
-		ci._2017metro, ci.wikipediametro,
+		ci._2017metro,
 		e.industryclassification, 
-		e.description, e."2017 GDP"
-		
+		e.description, e."2017 GDP",
+		s."NFL", s."MLB",
+		s."NBA", s."NHL",
+		a."Team", a."League",
+		a."Year", a."Home_GMS",
+		a."Road_GMS", a."Over_GMS",
+		a."Home_AVG", a."Road_AVG",
+		a."Over_AVG", a."Home_PCT",
+		a."Road_PCT", a."Over_PCT"
 FROM demo AS d
 INNER JOIN cities AS ci ON
-ci.state = d."State" and ci.city = d."City"
+ci.city = d."City" and ci.state = d."State"
 INNER JOIN econ AS e ON
 e.geoname = ci._2017metro
-		
+INNER JOIN sport_city AS s ON
+s."State" = d."State"
+INNER JOIN attendance AS a ON
+a."City" = d."City"
+;
+
